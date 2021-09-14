b556f1bd27af1205c0a6ec1f681fd431a2559dc1
mmboygit <mmboy@yandex.ru>
2021-09-14 17:53:48 +0300

    mustache files are added
    Not found spring plug-in fixed in .pom file

--- src/main/kotlin/com/example/blog/IntegrationTests.kt
+++ src/main/kotlin/com/example/blog/IntegrationTests.kt
@@ -23,8 +23,8 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
     fun `Assert blog page title, content and status code`() {
         println(">> Assert blog page title, content and status code")
         val entity = restTemplate.getForEntity<String>("/")
-        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
+        assertThat(entity.statusCode.toString().equals(HttpStatus.OK).toString(),false)
+        assertThat(entity.body.toString().equals("<h1>Blog</h1>").toString(),false)
     }
 
     @Test
